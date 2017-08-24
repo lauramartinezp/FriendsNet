0b581c41c284bac0aa78f7c2240c9b7e8794ce30
Laura <lauramartinezp@outlook.es>
2017-08-24 16:38:03 +0200

    commit funciona

---
+++ friendsnet/src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/dao/AbstractDAO.java
@@ -0,0 +1,11 @@
+package com.everis.alicante.courses.beca.summer17.friendsnet.dao;
+
+import java.io.Serializable;
+
+import com.everis.alicante.courses.beca.summer17.friendsnet.entity.FNEntity;
+
+public abstract class AbstractDAO<E extends FNEntity, ID extends Serializable> implements EntityDAO<E, ID> {
+	
+	
+
+}
---
+++ friendsnet/src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/dao/EntityDAO.java
@@ -0,0 +1,5 @@
+package com.everis.alicante.courses.beca.summer17.friendsnet.dao;
+
+public interface EntityDAO<T1, T2> {
+
+}
